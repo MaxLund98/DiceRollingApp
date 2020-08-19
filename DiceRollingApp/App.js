e31299aac4f6d604f498deaa5125542f3cccf921
MaxLund98 <64385226+MaxLund98@users.noreply.github.com>
2020-08-18 21:13:37 -0500

    Added rerolling and dropping functionality

--- DiceRollingApp/App.js
+++ DiceRollingApp/App.js
@@ -1,26 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView, CheckBox } from 'react-native';
 import { FontAwesome5 } from "@expo/vector-icons";
+
 export default function App() {
   const [numDice, changeNumDice] = React.useState(0);
   const [numSides, changeNumSides] = React.useState(0);
   const [results, changeResults] = React.useState([]);
   const [totalVal, changeTotalVal] = React.useState(0);
+  const [addMod, changeMod] = React.useState(0);
+  const [rerollVal, changeRerollVal] = React.useState(0);
+  const [rerollSelected, changeReroll] = React.useState(false);
+  const [dropSelected, changeDrop] = React.useState(false);
+  let dropMinFound = false;
 
-  console.log(numDice);
-  console.log(numSides);
+  console.log(rerollSelected);
+  console.log(dropSelected);
 
   function rollDice() {
     const results = [];
     let totalVal = 0;
+    let dropVal = 0;
     for (let i = 0; i < numDice; i++) {
-      const result = parseInt(Math.random()*numSides, 10) + 1;
+      let result = parseInt(Math.random()*numSides, 10) + 1;
+      if (rerollSelected) {
+        while (result === parseInt(rerollVal)) {
+          result = parseInt(Math.random()*numSides, 10) + 1;
+        }
+      }
       totalVal = totalVal + result;
       results.push(result);
     }
+    dropVal = Math.min(...results);
+    if (dropSelected) {
+      totalVal= totalVal-dropVal;
+    }
     changeResults(results);
-    changeTotalVal(totalVal);
+    changeTotalVal(totalVal+parseInt(addMod));
     console.log(results);
     console.log(totalVal);
   }
@@ -31,26 +47,80 @@ export default function App() {
       <Text style={styles.title}>Max's Dice App</Text>
       <FontAwesome5 name="dice-d20" size={75} color={"white"} />
       <View style={styles.textBoxView}> 
-      <TextInput
-      keyboardType = 'numeric' 
-      onChangeText = {(tu)=> changeNumDice(tu)} 
-      style={styles.textInput} />
-      <Text style={styles.title}>d</Text>
-      <TextInput
-      keyboardType = 'numeric' 
-      onChangeText = {(tx)=> changeNumSides(tx)}
-      style={styles.textInput} />
-      <StatusBar style="auto" /> 
+        <TextInput
+          keyboardType = 'numeric' 
+          onChangeText = {(tu)=> changeNumDice(tu)} 
+          style={styles.textInput} />
+        <Text style={styles.title}>d</Text>
+        <TextInput
+          keyboardType = 'numeric' 
+          onChangeText = {(tx)=> changeNumSides(tx)}
+          style={styles.textInput} />
+        <Text style={styles.title}> + </Text>
+        <TextInput
+          keyboardType = 'numeric'
+          onChangeText = {(addTx)=> changeMod(addTx)}
+          style={styles.textInput} />
       </View>
       <TouchableOpacity
-      style={styles.rollButton}
-      onPress = {rollDice}
+        style={styles.rollButton}
+        onPress = {rollDice}
       >
       <Text style={styles.buttonText}>Roll</Text>
       </TouchableOpacity>
-      {results.map((number) => (
-        <Text style={styles.title}>{number}</Text>
-      ))}
+      <View style={styles.textBoxView}>
+      <CheckBox
+        checkedIcon = 'dot-circle-o'
+        uncheckedIcon = 'circle-o'
+        checkedColor='black'
+        value={rerollSelected}
+        title='Select value to reroll:'
+        onChange={()=> changeReroll(!rerollSelected)}
+      />
+      <Text style={styles.label}>Select value to reroll:</Text>
+      {rerollSelected &&
+      <TextInput
+      keyboardType = 'numeric'
+      onChangeText = {(rrTx)=> changeRerollVal(rrTx)}
+      style={styles.textInput} />
+      }
+      </View>
+      <View style={styles.textBoxView}>
+       <CheckBox
+        checkedIcon = 'dot-circle-o'
+        uncheckedIcon = 'circle-o'
+        checkedColor='black'
+        value={dropSelected}
+        title='Drop lowest value'
+        onChange={()=> changeDrop(!dropSelected)}
+      />
+      <Text style={styles.label}>Drop lowest value</Text>
+      </View>
+      {dropSelected ? (
+      <View>
+        {results.map((number) => (
+          <View>
+            {!dropMinFound && number === Math.min(...results) ? (
+              <View>
+              <Text style={styles.drop}>{number}</Text>
+              {dropMinFound = true}
+              </View>
+            ) :
+            (
+              <Text style={styles.title}>{number}</Text>
+            )
+            }
+          </View>
+        ))}
+      </View>
+      ) : (
+        <View>
+        {results.map((number) => (
+          <Text style={styles.title}>{number}</Text>
+        ))}
+        </View>
+      )
+      }
       {totalVal > 0 && 
         <Text style={styles.title}>Total: {totalVal}</Text>
       }
@@ -72,6 +142,16 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: "bold",
     marginTop: 25,
+    color: "white",
+  },
+  drop: {
+    fontSize: 30,
+    fontWeight: "italic",
+    marginTop: 25,
+    color: "red",
+  },
+  label: {
+    fontSize: 16,
     color: "white",
   },
   textInput: {
